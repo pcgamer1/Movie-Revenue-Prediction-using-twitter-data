1dc1490c6fba473be63e4ca2890c05bab99e49dd
Sarthak Saxena <saxenasarthak72@gmail.com>
2019-04-03 02:29:26 +0530

    Update twittersentimentanalysis.py

--- twittersentimentanalysis.py
+++ twittersentimentanalysis.py
@@ -14,43 +14,49 @@ class TwitterClient(object):
     
     def __init__(self):
         
-        ck='VAB783khHiAX1hDMwmoeEoUPH' 
-        csk='vVriQ1bxRo50xtWtB4vw6q5O3ihiIu2HBpRQtcFU5SCARuUZM2' 
-        atk='3228432704-tNilRZxhI5C7mDdOi0HffWmp3jm9ImdF2AgcdgY' 
-        atsk='RNzXwB3hlUCbCYlNFfFJnXxpXkEAaS2WToTMhwcIFbiH1' 
+        ck='your key' 
+        csk='your key' 
+        atk='your key' 
+        atsk='your key'
         
         try:
             self.auth = OAuthHandler(ck, csk)
             self.auth.set_access_token(atk, atsk) 
-            # create tweepy API object to fetch tweets 
+            #Creating tweepy API object to fetch tweets 
             self.api = tweepy.API(self.auth) 
         except: 
             print("Error: Authentication Failed") 
             
     def clean_tweet(self, tweet): 
         ''' 
-        Utility function to clean tweet text by removing links, special characters 
-        using simple regex statements. 
+        Function to clean tweet text by removing links, special characters 
+        using regex statements. 
         '''
         return re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet) 
    
   
     def get_tweet_sentiment(self, tweet): 
         ''' 
-        Utility function to classify sentiment of passed tweet 
+        Function to return sentiment polarity of the passed tweet 
+        using textblob's sentiment attribute 
+        '''
+        #Calling TextBlob on passed tweet 
+        analysis = TextBlob(self.clean_tweet(tweet)) 
+        #Returning polarity
+        return analysis.sentiment.polarity
+    
+    
+     def get_tweet_subjectivity(self, tweet): 
+        ''' 
+        Function to return sentiment subjectivity of the passed tweet 
         using textblob's sentiment method 
         '''
-        # create TextBlob object of passed tweet text 
+        #Calling TextBlob on passed tweet  
         analysis = TextBlob(self.clean_tweet(tweet)) 
-        # set sentiment 
-        '''if analysis.sentiment.polarity > 0: 
-            return 'positive'
-        elif analysis.sentiment.polarity == 0: 
-            return 'neutral'
-        else: 
-            return 'negative'
-  '''
-        return analysis.sentiment.polarity
+        #Returning subjectivity
+        return analysis.sentiment.subjectivity
+    
+    
     def get_tweets(self, query, count = 10): 
         ''' 
         Main function to fetch tweets and parse them. 
@@ -58,7 +64,7 @@ class TwitterClient(object):
         tweets = [] 
   
         try: 
-            # call twitter api to fetch tweets 
+            #Calling api.search to fetch tweets 
             fetched_tweets = self.api.search(q=query,count=count) #since="2014-01-01",
                            #until="2014-02-01"
             
@@ -66,13 +72,14 @@ class TwitterClient(object):
                 
                 parsed_tweet = {} 
   
-                # saving text of tweet 
+                #Saving text of the tweet 
                 parsed_tweet['text'] = tweet.text 
-                # saving sentiment of tweet 
+                #Saving sentiment of the tweet 
                 parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text) 
-                if parsed_tweet['sentiment']==0:
-                    print(parsed_tweet['text'])
-                # appending parsed tweet to tweets list 
+                #Saving subjectivity of the tweet
+                parsed_tweet['subjectivity']= self.get_tweet_subjectivity(tweet.text)
+           
+                #Appending parsed tweet to tweets list 
                 if tweet.retweet_count > 0: 
                     # retweeet
                     if parsed_tweet not in tweets: 
@@ -80,7 +87,7 @@ class TwitterClient(object):
                 else: 
                     tweets.append(parsed_tweet) 
   
-            # return parsed tweets 
+            #Returning the parsed tweets 
             return tweets 
   
         except tweepy.TweepError as e: 
@@ -88,33 +95,47 @@ class TwitterClient(object):
             print("Error : " + str(e)) 
   
 def main():
-    # creating object of TwitterClient Class 
+    #Creating an object of TwitterClient Class 
     api = TwitterClient() 
-    # calling function to get tweets 
-    tweets = api.get_tweets(query="#kesari",count=500) 
-     # picking positive tweets from tweets 
-    npt=0
+    
+    #Calling member function to get tweets based on movie name
+    tweets = api.get_tweets(query="#kesari",count=500)
+    
+    #Picking positive tweets from tweets
     senttweet=[tweet['sentiment'] for tweet in tweets if tweet['sentiment']>=0]
+    
+    #Picking neutral tweets from tweets
+    neutraltweets=[tweet for tweet in tweets if tweet['sentiment']==0]
     npt=len(senttweet)
+    nnt=len(neutraltweets)
     print(npt)
-    finalsent=npt/len(tweets)
+    finalsent=npt/(len(tweets)-nnt)
     print(finalsent)
+    
+    #Importing the created Bollywood movie dataset
     import pandas as pd
     import numpy as np
     data=pd.read_excel("film.xlsx")
     data=data.replace([r"\bpoor\b",r"\baverage\b",r"\bgood\b"],[0.33,0.66,0.99],regex=True)
-
+    
+    #Assigning data
     x=data.drop(["revenue","name"],axis=1)
     y=data["revenue"]
-
+    
+    #Fitting the data to the model
     from sklearn.linear_model import LinearRegression
     lrm=LinearRegression()
     lrm.fit(x,y)
+    
+    #Preparing input
     finalsent*=5
     m=np.array([finalsent,0.99,0.66])
     M=m.reshape(1,-1)
+    
+    #Prediction
     r=lrm.predict(M)
     print(r)
-
+    
+#Calling the main function
 if __name__ == "__main__":
      main()
